917201aca10902123b380f564e122fcdc5f86298
Zheng Liang <zhliang@berkeley.edu>
2022-11-08 15:44:16 -0800

    neet a better understanding/formulation on the constraints to avoid infeasible situations

--- par.py
+++ par.py
@@ -13,9 +13,14 @@ from utils import grb_vars_shape, grb_vars_to_ndarray
 from EleNetX.mpdl import *
 from EleNetX.visualize import plot_ele_nx
 
+# Global Gurobi setting
+current_GMT = time.gmtime()
+timestamp = calendar.timegm(current_GMT)
+log_file = "gurobi.par.{}.log".format(timestamp)
+
 
 def get_par_constraints(X:gp.tupledict,
-                        # w:np.ndarray,
+                        w:np.ndarray,
                         max_size,
                         min_size) -> dict:
     """ Get partition constraints for each individual graph
@@ -29,9 +34,11 @@ def get_par_constraints(X:gp.tupledict,
     # (1) unique cluster assignment
     unique_assign = (X.sum(k, '*') == 1 for k in range(l))
     # (2) max size
-    size_ub = (X.sum('*', j) <= max_size for j in range(m))
+    size_ub = (gp.quicksum(X[k, j] * w[k] for k in range(l))
+               <= max_size for j in range(m))
     # (3) min size
-    size_lb = (X.sum('*', j) >= min_size for j in range(m))
+    size_lb = (gp.quicksum(X[k, j] * w[k] for k in range(l))
+               >= min_size for j in range(m))
 
     return {
         "Par:unique-subgraph-assignment"    :   unique_assign,
@@ -53,34 +60,38 @@ def get_par_objective(X:gp.tupledict,
     return cut_size
 
 
-def get_par_model(L, m=2, gamma=0.1):
+def get_par_model(L:sp.coo_matrix,
+                  w:np.ndarray,
+                  m:int,
+                  max_size,
+                  min_size):
     """
     """
     model = gp.Model("quadratic-programming graph partition")
 
     l, _ = L.shape
-    max_size = np.ceil(l / m) * (1 + gamma)
 
     # set variables
     X = model.addVars(l, m, vtype=GRB.BINARY)
 
     # set constraints
-    par_constrs = get_par_constraints(X, max_size, 0)
+    par_constrs = get_par_constraints(X, w=w,
+                                      max_size=max_size,
+                                      min_size=min_size)
     for constrs in par_constrs:
         model.addConstrs(par_constrs[constrs], name=constrs)
 
     # set objective
-    cut_size = get_par_objective(X, L)
+    cut_size = 0.0
+    cut_size += get_par_objective(X, L)
     model.setObjective(cut_size)
 
-    # Gurobi configuration
-    current_GMT = time.gmtime()
-    timestamp = calendar.timegm(current_GMT)
-    log_file = "gurobi.par.{}.log".format(timestamp)
+    # update model and Gurobi configuration
+    model.update()
     model.setParam("LogFile", log_file)
     model.setParam("LogToConsole", 0)
-
-    model.update()
+    model.setParam('TimeLimit', 20 * 60)
+    
     return model, X
 
 
@@ -101,11 +112,35 @@ if __name__ == "__main__":
     L = L.tocoo()
 
     l, _ = L.shape
-    m = 2
-    print(l)
+    max_size = l
+    min_size = 5
+    # assume node weights are almost balanced
+    # need to figure out when this problem will be infeasible
+    # with max and min area constraints for subgraphs
+    # since we have a minimum size constraint, we cannot automatic
+    # get some empty subgraphs, so we must enumerate m
+    m = int(np.floor(l / min_size))
+    m = min(l, m)
+    print(l, m)
 
-    model, X = get_par_model(L, m=4)
+    w = np.ones(l)
+
+    model, X = get_par_model(L, w=w, m=m, max_size=max_size, min_size=min_size)
     model.optimize()
 
-    X = grb_vars_to_ndarray(X).astype(int)
-    print(X)
+    if (model.status == GRB.OPTIMAL or
+        model.status == GRB.TIME_LIMIT or
+        model.status == GRB.NODE_LIMIT or
+        model.status == GRB.ITERATION_LIMIT or
+        model.status == GRB.USER_OBJ_LIMIT):
+        # get a solution
+        X = grb_vars_to_ndarray(X).astype(int)
+        print(X)
+        if model.status == GRB.TIME_LIMIT:
+            print("time limit")
+        if model.status == GRB.OPTIMAL:
+            print("get optimal")
+    elif (model.status == GRB.INFEASIBLE):
+        print("Infeasible")
+    else:
+        print("unknown error")
